7263356c2f3148a58a1a76e38a791253b51ab262
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-02-27 23:37:31 +0100

    se pondráa todo en minusculas

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,9 @@
 #ejercicio2: Análisis de una cadena de caracteres
 class palindromos():
+    palabra = str(input("Qué palabra desea comprobar si es un palíndromo?"))
+
+    i,j = "áéíóúñÁÉÍÓÚÑ" , "AEIOUÑaeiouñ"
+    acento = str. maketrans(i,j)
+    palabra = palabra.translate(acento)
+    palabra = palabra.lower()
     
