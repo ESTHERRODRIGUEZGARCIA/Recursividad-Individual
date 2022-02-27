ac2c7421f47121abda7dea1fae09dbd6b04a530e
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-02-27 23:29:35 +0100

    algo no vaaaa

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,13 +4,16 @@ print(palabras)
 
 class ejercicio():
     superior = len(palabras)
-        n_medio = (superior)//2
-        buscar_palabra = str(input("¿Qué palabra desea buscar?"))
+    n_medio = (superior)//2
+    buscar_palabra = str(input("¿Qué palabra desea buscar?"))
     def buscar(n_medio):
         n_tabla = palabras.index(buscar_palabra)
         if n_tabla == n_medio:
             print(f"{buscar_palabra} se encuentra en el índice {n_tabla}")
         elif n_tabla < n_medio:
             buscar(n_medio - 1)
+        elif n_tabla > n_medio:
+            buscar(n_medio + 1)
+    buscar(n_medio)
             
         
