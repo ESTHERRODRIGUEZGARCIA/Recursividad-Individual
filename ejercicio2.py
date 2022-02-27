2f21dd5a811411e6b092192c90000cbf7eb2dbd3
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-02-27 23:42:21 +0100

    cambios confirmados

--- ejercicio2.py
+++ ejercicio2.py
@@ -9,5 +9,14 @@ class palindromos():
     palabra = palabra.replace(" " , " ")
     lista = list(palabra)
     listafinal= list(reversed(palabra))
-    
+    def comprobar(palabra):
+        if len(palabra) < 1:
+            print(f" {palabra} es un palíndromo")
+        else:
+            if palabra[0] == palabra [-1]:
+                comprobar(palabra[1:-1])
+            else:
+                print(f"{palabra} no es un palíndromo")
+    comprobar(palabra)
+
     
