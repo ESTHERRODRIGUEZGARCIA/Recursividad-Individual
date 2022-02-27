78846917720d95dbc470588610e6172194c93ef9
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-02-27 23:15:28 +0100

    primer class

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,6 @@
 #ejercicio1 : 1. Búsqueda por dicotomía en una tabla ordenada
+palabras = ["comida", "baile", "disfrutar", "escalar", "corazón", "albóndigas"]
+print(palabras)
+
+class ejercicio():
+    
